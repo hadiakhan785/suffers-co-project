5e03f233627c340541b992ba335a2312d8aae8ac
hadiakhan785 <hadiakhan785@gmail.com>
2018-12-03 18:35:08 +0500

    half responsive

--- js/script.js
+++ js/script.js
@@ -20,21 +20,16 @@ function openNav() {
     document.querySelector('.collapse-nav').style['z-index'] = '5';
     document.querySelector('.collapse-nav').classList.remove('fadeOutRightBig');
     document.querySelector('.collapse-nav').classList.add('fadeInRightBig');
-    // document.querySelector('html').classList.add('full-height');            
-    // document.querySelector('body').classList.add('full-height');           
-    document.querySelector('body').style['overflow-y'] = 'hidden';  
-    // document.querySelector('body').style.position = 'fixed';            
+    document.querySelector('body').style.position = 'fixed';            
 
 }
 
+// close nav
 function closeNav(){
     document.querySelector('.collapse-nav').style.visibility = 'visible';
     document.querySelector('.collapse-nav').style['z-index'] = '-5';
     document.querySelector('.collapse-nav').classList.remove('fadeInRightBig');
-    document.querySelector('.collapse-nav').classList.add('fadeOutRightBig');
-    // document.querySelector('html').classList.remove('full-height');            
-    // document.querySelector('body').classList.remove('full-height');            
-    document.querySelector('body').style['overflow-y'] = 'scroll';  
-    // document.querySelector('body').style.position = 'static';            
+    document.querySelector('.collapse-nav').classList.add('fadeOutRightBig');  
+    document.querySelector('body').style.position = 'static';            
 
 }
